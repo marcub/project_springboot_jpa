c4a2422f551429f53e90b231c43376b6843efeb3
Marcus Cezar <marcusferraz98@gmail.com>
2022-10-19 11:14:58 -0300

    Project created

---
+++ src/main/java/com/javacourse/project_springboot/ProjectSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.javacourse.project_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpringbootApplication.class, args);
+	}
+
+}
