da456178bc391eaa4a4d529fdeccf65e792b1ec4
Marcus Cezar <marcusferraz98@gmail.com>
2022-10-21 22:12:15 -0300

    Implemented User Service layer and components registration

---
+++ src/main/java/com/javacourse/project_springboot/services/UserService.java
@@ -0,0 +1,25 @@
+package com.javacourse.project_springboot.services;
+
+import com.javacourse.project_springboot.entities.User;
+import com.javacourse.project_springboot.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
