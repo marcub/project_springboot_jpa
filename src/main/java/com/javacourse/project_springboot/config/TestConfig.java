a4d766fee1bf00bc981c43c4e3541e85f7e02db3
Marcus Cezar <marcusferraz98@gmail.com>
2022-10-27 22:31:50 -0300

    Implemented Product Entity/Service/Resource/Repository

--- src/main/java/com/javacourse/project_springboot/config/TestConfig.java
+++ src/main/java/com/javacourse/project_springboot/config/TestConfig.java
@@ -2,10 +2,12 @@ package com.javacourse.project_springboot.config;
 
 import com.javacourse.project_springboot.entities.Category;
 import com.javacourse.project_springboot.entities.Order;
+import com.javacourse.project_springboot.entities.Product;
 import com.javacourse.project_springboot.entities.User;
 import com.javacourse.project_springboot.entities.enums.OrderStatus;
 import com.javacourse.project_springboot.repositories.CategoryRepository;
 import com.javacourse.project_springboot.repositories.OrderRepository;
+import com.javacourse.project_springboot.repositories.ProductRepository;
 import com.javacourse.project_springboot.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -25,6 +27,8 @@ public class TestConfig implements CommandLineRunner {
     private OrderRepository orderRepository;
     @Autowired
     private CategoryRepository categoryRepository;
+    @Autowired
+    private ProductRepository productRepository;
 
 
     @Override
@@ -41,8 +45,16 @@ public class TestConfig implements CommandLineRunner {
         Category cat2 = new Category(null, "Books");
         Category cat3 = new Category(null, "Computers");
 
+        Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+        Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+        Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+        Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+        Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+
+
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
         categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
     }
 }
