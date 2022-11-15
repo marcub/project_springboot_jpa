5f56219d9dd5f87eab4e8be61b4f1c5c38c25d7f
Marcus Cezar <marcusferraz98@gmail.com>
2022-11-15 19:55:22 -0300

    Implemented user insert method

--- src/main/java/com/javacourse/project_springboot/resources/UserResource.java
+++ src/main/java/com/javacourse/project_springboot/resources/UserResource.java
@@ -4,11 +4,10 @@ import com.javacourse.project_springboot.entities.User;
 import com.javacourse.project_springboot.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -29,4 +28,11 @@ public class UserResource {
         User user = service.findById(id);
         return ResponseEntity.ok().body(user);
     }
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User user) {
+        User obj =  service.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
+
 }
