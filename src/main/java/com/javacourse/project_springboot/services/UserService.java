2694ab28f56d8531e02c5c314c13417a77db6a83
Marcus Cezar <marcusferraz98@gmail.com>
2022-11-18 10:38:28 -0300

    Implemented user exception handling - delete

--- src/main/java/com/javacourse/project_springboot/services/UserService.java
+++ src/main/java/com/javacourse/project_springboot/services/UserService.java
@@ -2,8 +2,12 @@ package com.javacourse.project_springboot.services;
 
 import com.javacourse.project_springboot.entities.User;
 import com.javacourse.project_springboot.repositories.UserRepository;
+import com.javacourse.project_springboot.services.exceptions.DatabaseException;
 import com.javacourse.project_springboot.services.exceptions.ResourceNotFoundException;
+import org.hibernate.dialect.Database;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +33,15 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        }
+        catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User user) {
